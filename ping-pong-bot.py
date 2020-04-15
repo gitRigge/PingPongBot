0856504ce4e836e99bdb4e6dfde321be836724ab
gitRigge <gitRigge@users.noreply.github.com>
2020-04-15 18:45:31 +0200

    Add files via upload

---
+++ ping-pong-bot.py
@@ -0,0 +1,40 @@
+import twitter
+import re
+
+api = twitter.Api(
+    consumer_key='xxxxx',
+    consumer_secret='xxxxx',
+    access_token_key='xxxxx',
+    access_token_secret='xxxxx'
+)
+
+file = open("last_id","r")
+id_str = file.read()
+file.close()
+try:
+    id = int(id_str)
+except:
+    id=None
+print('Ping-Pong-Bot: since_id = {}'.format(str(id)))
+
+mentions = api.GetMentions(count=None, since_id=id, max_id=None, trim_user=False, contributor_details=False, include_entities=True, return_json=True)
+print('Ping-Pong-Bot: number of mentions = {}'.format(len(mentions)))
+
+for mention in mentions:
+    text = text = mention['text']
+    id = mention['id']
+    tags = {tag.strip("#") for tag in text.split() if tag.startswith("#")}
+    print('Ping-Pong-Bot: current id = {}'.format(str(mention['id'])))
+    if 'ping' in tags:
+        print('Ping-Pong-Bot: mention contains "ping" hashtag')
+        screen_name = mention['user']['screen_name']
+        retweet_text = '@{} Here is your #pong :-)'.format(screen_name)
+        print('Ping-Pong-Bot: retweet')
+        retweet = api.PostUpdates(retweet_text, in_reply_to_status_id=id)
+    else:
+        print('Ping-Pong-Bot: mention does not contain "ping" hashtag')
+
+file = open("last_id","w")
+file.write(str(id))
+file.close()
+print('Ping-Pong-Bot: file written and closed')
